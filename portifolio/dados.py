dcd4f9ed841d507f085be57ef49de79a35aff1b1
Henrick <ricknogueira1231@gmail.com>
2025-05-11 19:02:48 -0300

    Primeira view

---
+++ portifolio/dados.py
@@ -0,0 +1,11 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Python'
+    },
+    'habilidade2': {
+        'nome': 'Django'
+    },
+    'habilidade3': {
+        'nome': 'Flask'
+    },
+}
--- portifolio/views.py
+++ portifolio/views.py
@@ -1,1 +1,4 @@
 from django.shortcuts import render
+from .dados import habilidades
+def home(request):
+    return render(request, 'home.html', {'habilidades': habilidades})
